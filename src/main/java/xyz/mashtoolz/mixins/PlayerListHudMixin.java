1be0c35cd48522dbd26755660533432ecd0bc0f8
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-05-27 13:21:59 -0400

    added: tab list modification

---
+++ src/main/java/xyz/mashtoolz/mixins/PlayerListHudMixin.java
@@ -0,0 +1,22 @@
+package xyz.mashtoolz.mixins;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.PlayerListHud;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ScoreboardObjective;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerListHud.class)
+public class PlayerListHudMixin  {
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void onRender(DrawContext context, int scaledWindowWidth, Scoreboard scoreboard, ScoreboardObjective objective, CallbackInfo ci) {
+
+        context.getMatrices().translate(0, 50, 0);
+    }
+}
