19727c41a1e25e5447d6f2ad26d98526826bf4fe
Sakurasou <realmashiroshiina@gmail.com>
2024-05-28 23:31:09 +0200

    Removed commented code for now; Fixed null checks & Server IP check

--- src/main/java/xyz/mashtoolz/FaceLift.java
+++ src/main/java/xyz/mashtoolz/FaceLift.java
@@ -3,38 +3,29 @@ package xyz.mashtoolz;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.Perspective;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.DisplayEntity.TextDisplayEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.network.ClientConnection;
-import net.minecraft.util.Hand;
 import xyz.mashtoolz.config.Config;
 import xyz.mashtoolz.helpers.ArenaTimer;
 import xyz.mashtoolz.helpers.DPSMeter;
 import xyz.mashtoolz.helpers.HudRenderer;
 import xyz.mashtoolz.helpers.KeyHandler;
 
-import java.sql.Time;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class FaceLift implements ClientModInitializer {
 
 	private static FaceLift instance;
-	private static final String ip = "beta.face.land";
-	//private static final String skillCommand = "skills";
 	public MinecraftClient client;
+	public ClientPlayerEntity player;
 	public Config config;
 	public DPSMeter dpsMeter;
 	public KeyHandler keyHandler;
@@ -42,14 +33,6 @@ public class FaceLift implements ClientModInitializer {
 	public HudRenderer hudRenderer;
 
 	private final HashMap<String, TextDisplayEntity> textDisplayEntities = new HashMap<>();
-	//private boolean rightMouseClickedLastTick = false;
-	//private Time throwTime = null;
-	//private Time reelTime = null;
-	//private final HashMap<Long, Integer> oneMinFish = new HashMap<>();
-	                     //<TIME, EXP>
-	//private final Pattern fishingXPRegex = Pattern.compile("Gained Fishing XP! \\(\\+(\\d+)XP\\)");
-
-
 
 	@Override
 	public void onInitializeClient() {
@@ -64,21 +47,20 @@ public class FaceLift implements ClientModInitializer {
 		hudRenderer = new HudRenderer();
 
 		ClientEntityEvents.ENTITY_LOAD.register((entity, world) -> {
-
 			switch (entity.getName().getString()) {
 				case "Text Display": {
 					textDisplayEntities.put(entity.getUuid().toString(), (TextDisplayEntity) entity);
 					break;
 				}
-            }
-
-
+			}
 		});
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 
 			if (client == null || client.player == null)
 				return;
+
+			player = client.player;
 
 			FallDamageCheck();
 			DPSNumbersCheck();
@@ -97,9 +79,6 @@ public class FaceLift implements ClientModInitializer {
 			if (config.mountKey.wasPressed())
 				keyHandler.onMountKey();
 
-			// if (config.potionKey.wasPressed())
-			// keyHandler.onPotionKey(1);
-
 			if (config.spell1Key.wasPressed())
 				keyHandler.onSpell1Key();
 
@@ -112,39 +91,9 @@ public class FaceLift implements ClientModInitializer {
 			if (config.spell4Key.wasPressed())
 				keyHandler.onSpell4Key();
 
-			/*if(config.xpCalculator.enabled) {
-				boolean rightMouseClickedThisTick = client.mouse.wasRightButtonClicked();
-				if (rightMouseClickedThisTick && !rightMouseClickedLastTick) {
-					Hand hand = client.player.getActiveHand();
-					ItemStack heldItem = client.player.getStackInHand(hand);
-
-					if (heldItem.getItem() == Items.FISHING_ROD) {
-						if (client.player.fishHook != null && client.player.fishHook.isInOpenWater()) { // Reeling
-							// doing nothing here (for now) because autoFishing rods wouldn't work otherwise.
-						} else { // Throwing
-							throwTime = new Time(System.currentTimeMillis());
-						}
-					}
-				}
-				//player.getInventory();
-				if (throwTime != null && reelTime != null) {
-					handleReelTime();
-				}
-
-				rightMouseClickedLastTick = rightMouseClickedThisTick;
-			}*/
-			if(config.arenaTimer.enabled && arenaTimer.isActive()) {
-				PlayerEntity player = MinecraftClient.getInstance().player;
-				if (player != null && player.getHealth() <= 0) {
-					arenaTimer.end();
-				}
-			}
+			if (config.arenaTimer.enabled && arenaTimer.isActive() && (player != null && player.getHealth() <= 0))
+				arenaTimer.end();
 		});
-
-		/*ClientReceiveMessageEvents.CHAT.register((client, sender, message, messageType, UUID) -> {
-            String messageText = Objects.requireNonNull(message).toString();
-			handleChatMessage(messageText);
-		});*/
 
 		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
 			client.execute(() -> {
@@ -152,93 +101,40 @@ public class FaceLift implements ClientModInitializer {
 				if (connection != null && connection.getAddress() != null) {
 					String serverAddress = connection.getAddress().toString();
 					System.out.println("Server IP: " + serverAddress);
-					if (serverAddress.contains(ip)) {
-						System.out.println("Joined faceland <3");
-						//client.execute(this::skillCheck);
+					if (serverAddress.toLowerCase().contains("face.land")) {
+						System.out.println("Joined FaceLand <3");
 					} else {
-						System.out.println("THAT'S NOT FACELAND!!!");
+						System.out.println("Not on FaceLand :(");
 					}
 				}
 			});
 		});
-
 
 		HudRenderCallback.EVENT.register((context, delta) -> {
 			hudRenderer.onHudRender(context, delta);
 		});
 	}
 
-	/*private void skillCheck() {
-		MinecraftClient client = MinecraftClient.getInstance();
-		if (client == null || client.player == null) {
-			System.out.println("[skillCheck] Client || Player is null");
-			return;
-		}
-		client.player.networkHandler.sendCommand(skillCommand);
-	}
-
-	private void handleChatMessage(String message) {
-		if(config.xpCalculator.enabled) {
-			Matcher matcher = fishingXPRegex.matcher(message);
-			if (matcher.find()) {
-				reelTime = new Time(System.currentTimeMillis());
-				System.out.println("[reelCheck] check");
-				handleReelTime();
-			}
-			if(afkFishingCheck()){
-				throwTime = new Time(System.currentTimeMillis());
-				System.out.println("[skillCheck] afkFishing");
-			}
-		}
-	}
-
-	private boolean afkFishingCheck() {
-		String[] afkRodNames = {"Automatic Fishing Rod", "Bewitched Fishing Rod", "Lazy Fishing Rod"};
-		ItemStack heldItem = Objects.requireNonNull(client.player).getStackInHand(client.player.getActiveHand());
-
-		if (heldItem == null || heldItem.isEmpty())
-			return false;
-
-		String itemName = heldItem.getName().getString();
-
-		for (String rodName : afkRodNames) {
-			if (itemName.contains(rodName)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-	private void handleReelTime() {
-		if (throwTime != null && reelTime != null) {
-			long reelDuration = (reelTime.getTime() - throwTime.getTime()) / (1000 * 60);
-			System.out.println("reelDuration: " + reelDuration);
-			oneMinFish.put(reelDuration, 1);
-			throwTime = null;
-			reelTime = null;
-		}
-	}*/
-
 	public static FaceLift getInstance() {
 		return instance;
 	}
 
 	public boolean isMounted() {
-		Entity ridingEntity = Objects.requireNonNull(client.player).getVehicle();
-		return ridingEntity != null && ridingEntity != client.player;
+		Entity ridingEntity = player.getVehicle();
+		return ridingEntity != null && ridingEntity != player;
 	}
 
 	private void FallDamageCheck() {
-		if (config.hurtTime == 0 && Objects.requireNonNull(client.player).hurtTime != 0)
-			config.hurtTime = client.player.hurtTime;
+		if (config.hurtTime == 0 && player.hurtTime != 0)
+			config.hurtTime = player.hurtTime;
 
-		if (config.hurtTime == -1 && Objects.requireNonNull(client.player).hurtTime == 0)
+		if (config.hurtTime == -1 && player.hurtTime == 0)
 			config.hurtTime = 0;
 
 		if (config.hurtTime > 0) {
 			config.hurtTime = -1;
 
-			var recentDamageSource = Objects.requireNonNull(client.player).getRecentDamageSource();
+			var recentDamageSource = player.getRecentDamageSource();
 			if (recentDamageSource != null && !recentDamageSource.getType().msgId().toString().equals("fall"))
 				config.lastHurtTime = System.currentTimeMillis();
 		}
--- src/main/java/xyz/mashtoolz/config/Category_XPCalc.java
+++ src/main/java/xyz/mashtoolz/config/Category_XPCalc.java
@@ -3,12 +3,10 @@ package xyz.mashtoolz.config;
 import org.joml.Vector2i;
 
 public class Category_XPCalc {
-    public boolean enabled = true;
-    public Vector2i position = new Vector2i(5, 99);
+	public boolean enabled = true;
+	public Vector2i position = new Vector2i(5, 99);
 
-    public static Category_XPCalc getDefault() {
-        return new Category_XPCalc();
-    }
-
+	public static Category_XPCalc getDefault() {
+		return new Category_XPCalc();
+	}
 }
-
