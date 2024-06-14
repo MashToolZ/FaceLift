3ac1e4c5c685eeb5c70dba8bf68889768b6f5295
Sakurasou <realmashiroshiina@gmail.com>
2024-06-14 21:31:19 +0200

    Added unicodes for Dodge/Block

--- src/main/java/xyz/mashtoolz/config/Category_CombatTimer.java
+++ src/main/java/xyz/mashtoolz/config/Category_CombatTimer.java
@@ -1,11 +1,15 @@
 package xyz.mashtoolz.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.joml.Vector2i;
 
 public class Category_CombatTimer {
 
 	public boolean enabled = true;
 	public Vector2i position = new Vector2i(5, 5);
+	public ArrayList<String> unicodes = new ArrayList<>(Arrays.asList("丞", "丟"));
 
 	public static Category_CombatTimer getDefault() {
 		return new Category_CombatTimer();
