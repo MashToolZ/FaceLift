9918d63eb62ddbbb070ecb146da9c5dcc49e28bf
Sakurasou <realmashiroshiina@gmail.com>
2024-09-03 17:21:21 +0200

    Fix searchbar closing when pressing inventory keybind

--- src/main/java/xyz/mashtoolz/mixins/HandledScreenMixin.java
+++ src/main/java/xyz/mashtoolz/mixins/HandledScreenMixin.java
@@ -4,14 +4,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.screen.slot.Slot;
+import xyz.mashtoolz.FaceLift;
 import xyz.mashtoolz.handlers.RenderHandler;
 
 @Mixin(HandledScreen.class)
 public abstract class HandledScreenMixin {
+
+	private static FaceLift instance = FaceLift.getInstance();
 
 	@Inject(method = "drawSlot", at = @At("HEAD"), cancellable = true)
 	public void FL_drawSlotHead(DrawContext context, Slot slot, CallbackInfo ci) {
@@ -26,5 +30,11 @@ public abstract class HandledScreenMixin {
 	@Inject(method = "render", at = @At("TAIL"))
 	public void FL_renderTail(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
 		RenderHandler.onHandledScreenRenderTail(context, mouseX, mouseY, delta);
+	}
+
+	@Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
+	public void FL_keyPressedHead(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+		if (RenderHandler.searchBar.isFocused() && instance.client.options.inventoryKey.matchesKey(keyCode, scanCode))
+			cir.setReturnValue(true);
 	}
 }
